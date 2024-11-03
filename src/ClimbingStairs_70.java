79e3cd2f2b487af56ecfcfdad941dd796dd90af6
akshayan <ak20004m@gmail.com>
2024-11-03 15:31:55 -0600

    Done coding

---
+++ src/ClimbingStairs_70.java
@@ -0,0 +1,18 @@
+public class ClimbingStairs {
+    //recursive solution 
+    public static int climbStairs(int n) {
+        if (n == 0 || n == 1){
+            return 1 ;
+        }
+        else{
+            return climbStairs(n-1) + climbStairs(n-2) ; 
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println(climbStairs(4));
+    }
+    
+
+
+}
